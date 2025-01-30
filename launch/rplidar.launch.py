f4ae374d8cafa0655e4977fc27838f230e2d552b
shoaf <smrbnsn5@memphis.edu>
2025-01-29 20:50:07 -0600

    added baud_rate and path serial path by-id

--- launch/rplidar.launch.py
+++ launch/rplidar.launch.py
@@ -11,7 +11,9 @@ def generate_launch_description():
             executable='rplidar_composition',
             output='screen',
             parameters=[{
-                'serial_port': '/dev/ttyUSB0',
+                'channel_type': 'serial',
+                'serial_port': '/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0',
+                'serial_baudrate': 115200,
                 'frame_id': 'laser_frame',
                 'angle_compensate': True,
                 'scan_mode': 'Standard'
