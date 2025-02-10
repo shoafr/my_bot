0adbb10e967881f9b08a0e7fa97dcc4310bb2561
shoaf <smrbnsn5@memphis.edu>
2025-02-10 10:13:07 -0600

    added start_trigger python files to test start LED setup

--- description/start_trigger_testing.py
+++ description/start_trigger_testing.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO
 import time
 
-# Setup
+# Setup stuff
 GPIO.setmode(GPIO.BCM)  # Use BCM numbering (GPIO17 instead of Pin 11)
 GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Set GPIO17 as input with pull-down
 
