95247fb558678bf5098cb59f3c83bf1a53142931
shoaf <smrbnsn5@memphis.edu>
2025-02-12 14:24:01 -0600

    deleted lidar_filter.yaml and reverted rplidar launch file to original - filter does not seem to work

--- launch/rplidar.launch.py
+++ launch/rplidar.launch.py
@@ -20,14 +20,5 @@ def generate_launch_description():
                 'scan_mode': 'Standard',
             }]
         ),
-        
-
-        Node(
-            package='laser_filters',
-            executable='scan_to_scan_filter_chain',
-            name='lidar_filter',
-            parameters=[os.path.join(get_package_share_directory('my_bot'), 'config', 'lidar_filter.yaml')],
-            remappings=[('/scan', '/filtered_scan')]
-        )
 
     ])
