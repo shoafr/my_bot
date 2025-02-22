6109791543fc7a2ce6e49cb42ba3cb76c6f6cbee
shoaf <smrbnsn5@memphis.edu>
2025-02-22 10:30:21 -0600

    modified to contain camera launch and disable move_to_tag script for now

--- launch/apriltag_navigation.launch.py
+++ launch/apriltag_navigation.launch.py
@@ -10,18 +10,25 @@ from ament_index_python.packages import get_package_share_directory
 def generate_launch_description():
     package_name = 'my_bot'  # Change this if your package name is different
 
-    # Include the existing launch_robot launch file (ensures motors & required nodes start)
+    # Include launch_robot.launch.py (motors, robot_state_publisher, twist_mux, etc.)
     launch_robot = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([
             os.path.join(get_package_share_directory(package_name), 'launch', 'launch_robot.launch.py')
         ])
     )
 
+    # Include camera.launch.py (camera node)
+    camera_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            os.path.join(get_package_share_directory(package_name), 'launch', 'camera.launch.py')
+        ])
+    )
+    
+
     # AprilTag detection node (from apriltag_ros package)
     apriltag_node = Node(
         package='apriltag_ros',
         executable='apriltag_node',
-        name='apriltag_node',
         output='screen',
         parameters=[os.path.join(get_package_share_directory('apriltag_ros'), 'cfg', 'tags_36h11.yaml')],
         remappings=[
@@ -30,16 +37,17 @@ def generate_launch_description():
         ]
     )
 
-    # Movement node that listens to AprilTag detections
-    move_to_tag = Node(
-        package=package_name,
-        executable='move_to_tag.py',
-        name='move_to_tag',
-        output='screen'
-    )
+    # # Movement node that listens to AprilTag detections
+    # move_to_tag = Node(
+    #     package=package_name,
+    #     executable='move_to_tag.py',
+    #     name='move_to_tag',
+    #     output='screen'
+    # )
 
     return LaunchDescription([
         launch_robot,
-        apriltag_node,
-        move_to_tag
+        camera_launch,
+        apriltag_node
+        # move_to_tag
     ])
